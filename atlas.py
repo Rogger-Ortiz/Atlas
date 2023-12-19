3158a5db58447d3f61382b95cf4da145ec9dd4fb
Rogger-Ortiz <contactme@rjortiz.com>
2023-12-19 16:32:44 -0500

    Help Function

--- atlas.py
+++ atlas.py
@@ -7,4 +7,22 @@ import asyncio
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+# Removes help command so we can use our own
+bot.remove_command('help')
+
+# Define all of the cogs here (comment out what you won't use)
+cogs = [
+    'cogs.test',
+    'cogs.help'
+]
+
+# Loads all of the cogs into the bot
+async def loadall():
+    for ext in cogs:
+        await bot.load_extension(ext)
+
+# Runs the function from before to load cogs
+asyncio.run(loadall())
+
+# Starts the bot
 bot.run(os.getenv("ATL_key"))
