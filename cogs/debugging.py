7085fc1fca05b3449c78c449622e912bc312cfe4
Rogger-Ortiz <contactme@rjortiz.com>
2023-12-24 13:42:20 -0500

    Remote update

--- cogs/debugging.py
+++ cogs/debugging.py
@@ -1,26 +1,48 @@
 
 import discord
 from discord.ext import commands
+import platform
+import os
 
 defaultEmbedColor=discord.Color(0xe67e22)
 green = discord.Color(0x00FF00)
 red = discord.Color(0xFF0000)
+
+# Check if I am the author
+def RJisAuthor(ctx):
+    return ctx.author.id == 248440677350899712
 
 class debugging(commands.Cog):
     description=""
     def __init__(self,bot):
         self.bot = bot
 
+
+
     # Setup the debugging "Shutdown" command
     @commands.command(name="shutdown", hidden=True)
     async def shutdown(self, ctx):
-        # Make sure I (RJ) am the author
-        if ctx.author.id != 248440677350899712:
-            return
-        else:
+        if RJisAuthor(ctx):
             # Quit
             await ctx.reply("Goodbye!")
             exit()
+        else:
+            return
+
+    # Setup remote updating of the bot
+    @commands.command(name="update", hidden=True)
+    async def update(self, ctx):
+         if RJisAuthor(ctx):
+              if platform.system() == "Windows":
+                   print("Can't update PTB this way!!!")
+                   return
+              if platform.system() == "Linux":
+                   ctx.reply("Updating, please allow me 5 seconds to reboot...")
+                   os.system(f"./updateATL {os.getpid()}")
+                   exit()
+         else:
+              return
+              
 
 async def setup(bot):
 	await bot.add_cog(debugging(bot))
