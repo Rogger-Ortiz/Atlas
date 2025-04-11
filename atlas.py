9376b211731a61209b1310d651c5b1c43eda349d
Rogger-Ortiz <contactme@rjortiz.com>
2025-04-11 15:09:12 -0400

    Added DotEnv support

--- atlas.py
+++ atlas.py
@@ -3,7 +3,9 @@ import discord
 from discord.ext import tasks, commands
 import nest_asyncio
 import asyncio
+from dotenv import load_dotenv
 
+load_dotenv()
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='$', intents=intents)
 
